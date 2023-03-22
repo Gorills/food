32b08ec016a0e64d8be0721c7003f968f01790d5
Gorills <gorivanickiy@gmail.com>
2023-03-22 08:44:52 +0700

    fix

--- main/admin/views.py
+++ main/admin/views.py
@@ -717,13 +717,15 @@ def order_status_change(request, pk):
     if request.method == 'POST':
         form = OrderForm(request.POST, instance=order)
         if form.is_valid():
-            form.save()
+            
 
 
             if loyalty_settings.active == True:
                 status = request.POST['status']
 
                 if status == 'Выполнен':
+
+                    
 
                     user = order.user_pr
                     card = LoyaltyCard.objects.get(user=user)
@@ -739,7 +741,23 @@ def order_status_change(request, pk):
 
                     card.save()
 
+                if status == 'Отказ':
 
+                    user = order.user_pr
+                    card = LoyaltyCard.objects.get(user=user)
+                    
+                    # Накапливаем сумму на карту лояльности
+                    card.summ = Decimal(card.summ) - (Decimal(order.summ) - Decimal(order.delivery_price))
+                    
+
+                    if loyalty_settings.status_up == True:
+
+                        card.balls = card.balls - (((Decimal(order.summ) - Decimal(order.delivery_price)) / 100) * card.status().percent_up).quantize(Decimal("1"), decimal.ROUND_DOWN) 
+
+
+                    card.save()
+
+            form.save()
 
             return redirect('order_detail', order.id)
 
