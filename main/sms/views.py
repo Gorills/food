7819c217fdce169243e1d66ad0a00a12ffa77473
Gorills <gorivanickiy@gmail.com>
2023-05-15 20:37:25 +0700

    fix

--- main/sms/views.py
+++ main/sms/views.py
@@ -3,12 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 import requests
 from setup.models import BaseSettings
-
 try:
     sms = BaseSettings.objects.get().sms
-    apikey = BaseSettings.objects.get().sms_pilot_apikey
 except:
     sms = False
+
+try:
+    apikey = BaseSettings.objects.get().sms_pilot_apikey
+except:
+    
     apikey = 'BS0H0F27LS92CU4G3YVZQ693A1QGCTQT2DBOF2NDKMB99465LQSU8O7RPU084Y60'
 
 
