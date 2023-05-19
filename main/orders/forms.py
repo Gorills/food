33cd7da28c71f1800973072226f9d83efbf754e4
Gorills <gorivanickiy@gmail.com>
2023-05-19 09:35:50 +0700

    fix

--- main/orders/forms.py
+++ main/orders/forms.py
@@ -5,7 +5,7 @@ from snowpenguin.django.recaptcha3.fields import ReCaptchaField
 
 
 class CallbackForm(forms.Form):
-    # captcha = ReCaptchaField()
+    captcha = ReCaptchaField()
     name = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'popup__input', 'placeholder': 'Имя'}))
     phone = forms.CharField(label='',widget=forms.TextInput(attrs={'class': 'popup__input phone', 'placeholder': 'Телефон'}))
     messages = forms.CharField(label='',widget=forms.Textarea(attrs={'class': 'popup__input', 'placeholder': 'Сообщение'}))
@@ -13,7 +13,7 @@ class CallbackForm(forms.Form):
 
 
 class OrderCreateForm(forms.ModelForm):
-    captcha = ReCaptchaField()
+    # captcha = ReCaptchaField()
     class Meta:
         model = Order
         fields = [
@@ -26,7 +26,7 @@ class OrderCreateForm(forms.ModelForm):
             'flat',
             'time',
             'order_conmment',
-            'captcha',
+            
             'delivery_method',
             'pay_method',
             'address',
