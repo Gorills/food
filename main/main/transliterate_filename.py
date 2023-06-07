36d7f3e14df932094b9402f3349d89cb3839c34b
Gorills <gorivanickiy@gmail.com>
2023-06-07 21:48:40 +0700

    fix

--- main/main/transliterate_filename.py
+++ main/main/transliterate_filename.py
@@ -8,7 +8,10 @@ def transliterate_file(instance, filename):
     # Get the file extension
     ext = filename.split('.')[-1]
     # Transliterate the file name without the extension
-    transliterated_name = translit(''.join(filename.split('.')[:-1]), reversed=True)
+    try:
+        transliterated_name = translit(''.join(filename.split('.')[:-1]), reversed=True)
+    except:
+        transliterated_name = filename
     # Form the new file name with the transliterated name and extension
     new_filename = f"{transliterated_name}.{ext}"
     # Return the new file name
