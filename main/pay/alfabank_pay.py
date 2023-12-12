aa5307e0529e70e55dfadfed95f825eac0ee37c5
Gorills <gorivanickiy@gmail.com>
2023-12-12 16:43:38 +0700

    fix

--- main/pay/alfabank_pay.py
+++ main/pay/alfabank_pay.py
@@ -116,7 +116,7 @@ def get_status(pay_id):
     while status == False and status_pay != 2:
         if status_pay == 6 or count == 48:
 
-            message = f'Статус оплаты: {status_pay}, сайт: {BaseSettings.objects.get().name}'
+            message = f'Статус оплаты: {status_pay}, сайт: {BaseSettings.objects.get().name}, Счетчик: {count}'
             telegram_bot_work = '5922674089:AAFxcjyYfti0ypSINOSP9jMz74RloWpmPPs'
             telegram_group_work = '-1001850576262'
             send_message(telegram_bot_work, telegram_group_work, message)
@@ -133,7 +133,7 @@ def get_status(pay_id):
 
             order_telegram(telegram_bot, telegram_group, order)
 
-            message = f'Статус оплаты: {status_pay}, сайт: {BaseSettings.objects.get().name}'
+            message = f'Статус оплаты: {status_pay}, сайт: {BaseSettings.objects.get().name}, Счетчик: {count}'
             telegram_bot_work = '5922674089:AAFxcjyYfti0ypSINOSP9jMz74RloWpmPPs'
             telegram_group_work = '-1001850576262'
             send_message(telegram_bot_work, telegram_group_work, message)
@@ -141,7 +141,7 @@ def get_status(pay_id):
         count +=1
         time.sleep(10)
 
-        message = f'Статус оплаты: {status_pay}, сайт: {BaseSettings.objects.get().name}'
+        message = f'Статус оплаты: {status_pay}, сайт: {BaseSettings.objects.get().name}, Счетчик: {count}'
         telegram_bot_work = '5922674089:AAFxcjyYfti0ypSINOSP9jMz74RloWpmPPs'
         telegram_group_work = '-1001850576262'
         send_message(telegram_bot_work, telegram_group_work, message)
