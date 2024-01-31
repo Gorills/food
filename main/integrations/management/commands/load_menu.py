d4f19e0e7259ce8ed01e6773e337cbc7e1153757
Gorills <gorivanickiy@gmail.com>
2024-01-31 20:22:34 +0700

    fix

---
+++ main/integrations/management/commands/load_menu.py
@@ -0,0 +1,8 @@
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+from integrations.iiko import load_menu
+
+class Command(BaseCommand):
+    help = 'Синхронизация каталогов с IIKO'
+    def handle(self, *args, **kwargs):
+        load_menu(False, False)
