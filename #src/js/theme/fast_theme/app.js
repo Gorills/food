1ce4c8c6acd658cca4ae09315c79447a6a936f29
Gorills <gorivanickiy@gmail.com>
2024-05-07 11:14:36 +0700

    fix

--- #src/js/theme/fast_theme/app.js
+++ #src/js/theme/fast_theme/app.js
@@ -1801,6 +1801,17 @@ $(document).on('click', '.order_create', function(e) {
                     localStorage.removeItem('cart');
                     localStorage.removeItem('lastOrder');
 
+                    // Получаем список всех кук с помощью document.cookie
+                    var cookies = document.cookie.split(";");
+
+                    // Итерируемся по всем кукам и устанавливаем их срок действия в прошлое время
+                    for (var i = 0; i < cookies.length; i++) {
+                        var cookie = cookies[i];
+                        var eqPos = cookie.indexOf("=");
+                        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+                        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/";
+                    }
+
 
                     
     
