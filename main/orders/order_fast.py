44fbf3f9f0c65895101c2f79cbc9a8bc060136df
Gorills <gorivanickiy@gmail.com>
2024-12-02 10:36:28 +0700

    fix

--- main/orders/order_fast.py
+++ main/orders/order_fast.py
@@ -473,8 +473,12 @@ def order_create(request):
     except Exception as e:
         summ = Decimal(json_order['summ'])
 
-        error_message = f'Сумма в заказе: {summ}'
-        send_message(wo_elegram_bot, wo_telegram_group, error_message)
+        error_message2 = f'Сумма в заказе: {summ}'
+        
 
-        error_message = f'Ошибка оформления заказа: {traceback.format_exc()}'
+        error_message = f'''
+                {error_message2}, 
+                Ошибка оформления заказа: {traceback.format_exc()}
+
+        '''
         send_message(wo_elegram_bot, wo_telegram_group, error_message)
