1dad4cd50216ff9c7c1a0755697b4707c6abcde0
Gorills <gorivanickiy@gmail.com>
2025-05-26 10:28:10 +0700

    fix

--- main/integrations/management/commands/load_menu.py
+++ main/integrations/management/commands/load_menu.py
@@ -1,8 +1,13 @@
 from django.core.management.base import BaseCommand
 from django.utils import timezone
 from integrations.iiko import load_menu
+from shop.models import PickupAreas
 
 class Command(BaseCommand):
     help = 'Синхронизация каталогов с IIKO'
     def handle(self, *args, **kwargs):
-        load_menu(True, True, None)
+        unique_api_keys = PickupAreas.objects.filter(api_key__isnull=False).values_list('api_key', flat=True).distinct()
+        for api_key in unique_api_keys:
+            area = PickupAreas.objects.filter(api_key=api_key).order_by('id').first()
+            if area:
+                load_menu(True, True, area)
